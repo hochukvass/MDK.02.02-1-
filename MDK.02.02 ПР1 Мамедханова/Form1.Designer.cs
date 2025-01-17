e0ea72efec2fc657c034ba7b986cc0f3c88f30b4
hochukvass <ataevaaydana@gmail.com>
2025-01-17 16:27:45 +0300

    Основной коммит

--- MDK.02.02 ПР1 Мамедханова/Form1.Designer.cs
+++ MDK.02.02 ПР1 Мамедханова/Form1.Designer.cs
@@ -28,39 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(313, 180);
+            button1.Font = new Font("Segoe UI", 30F);
+            button1.Location = new Point(279, 263);
             button1.Name = "button1";
-            button1.Size = new Size(140, 55);
+            button1.Size = new Size(247, 76);
             button1.TabIndex = 0;
-            button1.Text = "Click this";
+            button1.Text = "Крутить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(341, 259);
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(335, 368);
             label1.Name = "label1";
-            label1.Size = new Size(85, 20);
+            label1.Size = new Size(141, 35);
             label1.TabIndex = 1;
-            label1.Text = "Hello Word";
+            label1.Text = "пока гавно";
             label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.Location = new Point(24, 24);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 50);
+            button2.TabIndex = 2;
+            button2.Text = "Выход";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 40F);
+            label2.Location = new Point(262, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 89);
+            label2.TabIndex = 3;
+            label2.Text = "♥";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 40F);
+            label3.Location = new Point(352, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 89);
+            label3.TabIndex = 4;
+            label3.Text = "♥";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 40F);
+            label4.Location = new Point(442, 140);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 89);
+            label4.TabIndex = 5;
+            label4.Text = "♥";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(663, 24);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 31);
+            label5.TabIndex = 6;
+            label5.Text = "Счёт: 0";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(577, 98);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(211, 305);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(24, 98);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(216, 305);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            pictureBox2.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -69,5 +164,12 @@
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- MDK.02.02 ПР1 Мамедханова/Form1.cs
+++ MDK.02.02 ПР1 Мамедханова/Form1.cs
@@ -2,6 +2,9 @@ namespace MDK._02._02_ПР1_Мамедханова
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
+        int a, b, c;
+        long score = 0;
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +12,52 @@ namespace MDK._02._02_ПР1_Мамедханова
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Hellooo Wooorld!!";
+            a = rand.Next(9);
+            b = rand.Next(9);
+            c = rand.Next(9);
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            label2.Text = Convert.ToString(a);
+            label3.Text = Convert.ToString(b);
+            label4.Text = Convert.ToString(c);
+
+            if (a == 7 && b == 7 && c == 7)
+            {
+                label1.Text = "ЭЭЭОООДЖЕКПОТ!!!!";
+                score += 10000;
+                label5.Text = "Счёт: " + score;
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+            }
+            else if (a == b && b == c)
+            {
+                score += 7000;
+                label5.Text = "Счёт: " + score;
+                label1.Text = "уже лучше";
+            }
+            else if (a == b || b == c || a == c)
+            {
+                score += 3000;
+                label5.Text = "Счёт: " + score;
+                label1.Text = "крути дальше";
+            }
+            else
+            {
+                score -= 1000;
+                label5.Text = "Счёт: " + score;
+                label1.Text = "пока гавно";
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
