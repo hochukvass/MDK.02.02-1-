3100ff2424b8ab9903c1147dd43ed84b1e2ed4e2
хочу квас < @HOCHUKASS>
2025-01-17 14:41:05 +0300

    Добавьте файлы проекта.

---
+++ MDK.02.02 ПР1 Мамедханова/Form1.cs
@@ -0,0 +1,20 @@
+namespace MDK._02._02_ПР1_Мамедханова
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hellooo Wooorld!!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
